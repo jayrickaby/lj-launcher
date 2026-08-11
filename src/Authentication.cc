012f3781b4ce648638806dac1992413c73bf24e1
Jay <jayrickaby@pm.me>
2026-08-11 14:58:08 +0100

    feat(authentication): add basic authentication which gets pkce and login data

---
+++ src/Authentication.cc
@@ -0,0 +1,69 @@
+//
+// Created by jay on 11/08/2026.
+//
+
+#include "Authentication.h"
+
+Authentication::Authentication(QObject* parent) :
+QObject(parent),
+login_data_(getLoginData())
+{}
+
+LoginData Authentication::getLoginData() {
+  LoginData data;
+
+  PkceData pkce {generatePkceData()};
+  data.code_verifier = pkce.code_verifier;
+
+  data.state = generateToken(16);
+
+  QUrl url {kAuthUrl};
+  QUrlQuery query;
+  query.addQueryItem("client_id", kClientId);
+  query.addQueryItem("response_type", "code");
+  query.addQueryItem("redirect_uri", kRedirectUri.toString());
+  query.addQueryItem("response_mode", "query");
+  query.addQueryItem("scope", kScope);
+  query.addQueryItem("state", data.state);
+  query.addQueryItem("code_challenge", pkce.code_challenge);
+  query.addQueryItem("code_challenge_method", pkce.code_challenge_method);
+  url.setQuery(query);
+
+  data.url = url;
+
+  return data;
+}
+
+QString Authentication::generateToken(int length) const {
+  const QString allowed_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890-._~";
+  QString token;
+  token.reserve(length);
+
+  int allowed_length { static_cast<int>(allowed_characters.length()) };
+
+  for (int i = 0; i < length; i++) {
+    const int index {QRandomGenerator::global()->bounded(allowed_length)};
+    token.append(allowed_characters[index]);
+  }
+
+  return token;
+}
+
+PkceData Authentication::generatePkceData() const {
+  PkceData pkce_data;
+
+  QString token {generateToken(128)};
+  pkce_data.code_verifier = token;
+
+  QByteArray hash = QCryptographicHash::hash(
+    token.toLatin1(),
+    QCryptographicHash::Sha256
+    );
+
+  pkce_data.code_challenge = hash.toBase64(
+    QByteArray::Base64UrlEncoding
+    | QByteArray::OmitTrailingEquals
+    );
+
+  return pkce_data;
+}
---
+++ src/Authentication.h
@@ -0,0 +1,62 @@
+//
+// Created by jay on 11/08/2026.
+//
+
+#ifndef LJ_LAUNCHER_AUTHENTICATION_H_
+#define LJ_LAUNCHER_AUTHENTICATION_H_
+
+#include <iostream>
+
+#include <qqml.h>
+#include <QCryptographicHash>
+#include <QObject>
+#include <QRandomGenerator>
+#include <QUrlQuery>
+
+struct PkceData {
+  QString code_challenge;
+  QString code_challenge_method {"S256"};
+  QString code_verifier;
+};
+
+struct LoginData {
+  QString code_verifier;
+  QString state;
+  QUrl url;
+};
+
+struct UserData {
+  QString username;
+  QString uuid;
+  QString token;
+};
+
+class Authentication : public QObject {
+  Q_OBJECT
+  QML_ELEMENT
+  QML_SINGLETON
+
+public:
+  explicit Authentication(QObject *parent = nullptr);
+
+private:
+  LoginData getLoginData();
+  QUrl getLoginUrl() const;
+  PkceData generatePkceData() const;
+  QString generateToken(int length) const;
+
+  inline static const QString kClientId{"478514ce-2d7e-4e71-9301-29eb2241e2d6"};
+  inline static const QUrl kRedirectUri{"http://localhost"};
+
+  bool authenticated_ {false};
+
+  LoginData login_data_;
+  UserData user_data_;
+
+  inline static const QUrl kAuthUrl{"https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize"};
+  inline static const QUrl kTokenUrl{"https://login.microsoftonline.com/consumers/oauth2/v2.0/token"};
+  inline static const QString kScope{"XboxLive.signin offline_access"};
+
+};
+
+#endif  // LJ_LAUNCHER_AUTHENTICATION_H_
