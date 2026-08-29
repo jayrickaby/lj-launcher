04da1c8567e690960677b1bbc47dbd9d93056c41
Jay <jayrickaby@pm.me>
2026-08-29 23:28:28 +0100

    chore: unindent main

--- src/main.cc
+++ src/main.cc
@@ -7,60 +7,60 @@
   #include <QQmlApplicationEngine>
   #include <QQuickStyle>
 
-  #include "Application.h"
-  #include "ProfileEntry.h"
-  #include "Profiles.h"
-  #include "ProfilesTable.h"
-  #include "minecraft/exec/Game.h"
-  #include "minecraft/exec/JavaVirtualMachine.h"
-  #include "minecraft/ver/VersionManifest.h"
-  #include "net/Authentication.h"
-  #include "net/Downloader.h"
-  #include "net/Versions.h"
+#include "Application.h"
+#include "ProfileEntry.h"
+#include "Profiles.h"
+#include "ProfilesTable.h"
+#include "minecraft/exec/Game.h"
+#include "minecraft/exec/JavaVirtualMachine.h"
+#include "minecraft/ver/VersionManifest.h"
+#include "net/Authentication.h"
+#include "net/Downloader.h"
+#include "net/Versions.h"
 
-  int main(int argc, char *argv[]) {
-    qputenv("QT_FORCE_STDERR_LOGGING", "1");
-    QGuiApplication app(argc, argv);
+int main(int argc, char *argv[]) {
+  qputenv("QT_FORCE_STDERR_LOGGING", "1");
+  QGuiApplication app(argc, argv);
 
-    // Default to org.kde.desktop style unless the user forces another style
-    auto system {SystemInfo::getOperatingSystem()};
-    if (system.name == SystemName::LINUX and qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
-      // QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
-      QQuickStyle::setStyle("Fusion");
-    }
-
-    JavaVirtualMachine::setVariable("launcher_name", Application::getApplicationName());
-    JavaVirtualMachine::setVariable("launcher_version", Application::getApplicationVersion());
-
-    QGuiApplication::setApplicationName(Application::getApplicationName());
-    QGuiApplication::setOrganizationDomain(Application::getOrganisationDomain());
-    QGuiApplication::setOrganizationName(Application::getOrganisationName());
-
-    QGuiApplication::setWindowIcon(QIcon(Application::getDefaultIcon()));
-
-    ProfileManager::refreshProfiles();
-
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Application", Application::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Authentication", Authentication::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Downloader", Downloader::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Game", Game::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Launcher", Launcher::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Profiles", Profiles::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Versions", Versions::getInstance());
-    qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "VersionManifest", VersionManifest::getInstance());
-
-    qmlRegisterType<ProfilesTable>("jayrickaby.lj_launcher", 1, 0, "ProfilesTable");
-
-    QQmlApplicationEngine engine;
-    QObject::connect(
-      &engine, &QQmlApplicationEngine::objectCreationFailed,
-      &app, []() {
-        QCoreApplication::exit(-1);
-      },
-      Qt::QueuedConnection
-    );
-
-    engine.loadFromModule("jayrickaby.lj_launcher", "Main");
-
-    return QGuiApplication::exec();
+  // Default to org.kde.desktop style unless the user forces another style
+  auto system {SystemInfo::getOperatingSystem()};
+  if (system.name == SystemName::LINUX and qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE")) {
+    // QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    QQuickStyle::setStyle("Fusion");
   }
+
+  JavaVirtualMachine::setVariable("launcher_name", Application::getApplicationName());
+  JavaVirtualMachine::setVariable("launcher_version", Application::getApplicationVersion());
+
+  QGuiApplication::setApplicationName(Application::getApplicationName());
+  QGuiApplication::setOrganizationDomain(Application::getOrganisationDomain());
+  QGuiApplication::setOrganizationName(Application::getOrganisationName());
+
+  QGuiApplication::setWindowIcon(QIcon(Application::getDefaultIcon()));
+
+  ProfileManager::refreshProfiles();
+
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Application", Application::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Authentication", Authentication::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Downloader", Downloader::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Game", Game::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Launcher", Launcher::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Profiles", Profiles::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "Versions", Versions::getInstance());
+  qmlRegisterSingletonInstance("jayrickaby.lj_launcher", 1, 0, "VersionManifest", VersionManifest::getInstance());
+
+  qmlRegisterType<ProfilesTable>("jayrickaby.lj_launcher", 1, 0, "ProfilesTable");
+
+  QQmlApplicationEngine engine;
+  QObject::connect(
+    &engine, &QQmlApplicationEngine::objectCreationFailed,
+    &app, []() {
+      QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection
+  );
+
+  engine.loadFromModule("jayrickaby.lj_launcher", "Main");
+
+  return QGuiApplication::exec();
+}
